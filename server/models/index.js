b37b0f4cace54bfcd3c3a2cb564e991efb2e6b46
Lee Palacios <vitualizz@gmail.com>
2020-05-13 17:29:34 -0500

    remove logo & fix database

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const Sequelize = require('sequelize')
-const db = require('./config/database')
+const db = require('../config/database')
 
 fs.readdirSync(__dirname)
   .filter(file => file.indexOf(".") !== 0 && file !== "index.js")
