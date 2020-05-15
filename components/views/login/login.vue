227b7c9374b843d2d63bdebf3c13ebe0738ae94b
Lee Palacios <vitualizz@gmail.com>
2020-05-14 21:14:51 -0500

    login component

---
+++ components/views/login/login.vue
@@ -0,0 +1,33 @@
+<template lang='pug'>
+  el-form(:model="login")
+    el-form-item
+      el-input(type='email')
+    el-form-item
+      el-input(type='password')
+    el-form-item
+      btn.submit(color='green') Login
+</template>
+
+<script>
+import btn from '~/components/base/inputs/btn'
+
+export default {
+  components: {
+    btn
+  },
+  data () {
+    return {
+      login: {
+        email: '',
+        password: ''
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang='sass'>
+.submit
+  width: 130px
+  @extend .f-size-4
+</style>
--- pages/login.vue
+++ pages/login.vue
@@ -2,16 +2,33 @@
   .login-main.has-text-centered
     p.title GAST0S
     el-row
-      btn(color='green').btn Login
-      btn(color='yellow').btn Register
+      btn(
+        @click.native="loginVisible = true"
+        color='green'
+      ) Login
+      btn(color='yellow') Register
+    el-dialog(
+      title='Login'
+      :visible.sync='loginVisible'
+      width='20%'
+    )
+      Login
 </template>
 
 <script>
+import Login from '~/components/views/login/login'
 import btn from '~/components/base/inputs/btn'
 
 export default {
   components: {
+    Login,
     btn
+  },
+  data () {
+    return {
+      loginVisible: false,
+      registerVisible: false
+    }
   }
 }
 </script>
@@ -25,6 +42,9 @@ export default {
 .title
   font-size: 15vw
 
-.btn
-  @extend .f-size-5
+.el-row
+  &::v-deep button
+    width: 250px
+    margin: 0px 20px
+    @extend .f-size-5
 </style>
